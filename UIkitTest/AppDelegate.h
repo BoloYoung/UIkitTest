dc22129bb2514b90377e5d822cd6f8d3b8b95f48
youngyun <412794844@qq.com>
2013-07-10 10:11:16 +0800

    Initial Commit

---
+++ UIkitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIkitTest
+//
+//  Created by apple on 13-7-10.
+//  Copyright (c) 2013年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
